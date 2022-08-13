ec16d7a416ef35513483d8d1627d423fda92c492
Lenzzstore <101622758+Lenzzstore@users.noreply.github.com>
2022-08-13 15:20:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['6281511480762'],
-  ['6281511480762', 'YahyaGanzz', true]
+  ['6285236138574'],
+  ['6285236138574', 'GaaGanzz', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -59,7 +59,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = 'AzzBot-Yahya'
+global.wm = 'GaaGanz'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
